1d90fb252572805bc028ee4a71f406484bc8fc89
Kirsty Weitzel <kweitzel@rti.org>
2024-02-15 09:29:41 -0500

    move functions to utils

---
+++ utils.py
@@ -0,0 +1,25 @@
+import os
+
+def format_line(line):
+    # Remove index and commas from row
+    formatted_row = ' '.join(line.split(',')[1:])
+
+    # Add class label and perplexity to each row
+    formatted_row += " grave 0\n"
+
+    return formatted_row
+
+def process_file(input_file_path, output_folder):
+    try:
+        with open(input_file_path, 'r') as infile:
+            content = infile.readlines()
+
+        # Format each line and write to modified output file
+        output_file_path = os.path.join(output_folder, f"{os.path.basename(input_file_path)}")
+        with open(output_file_path, 'w') as outfile:
+            for line in content:
+                modified_line = format_line(line)
+                outfile.write(modified_line)
+
+    except FileNotFoundError:
+        print(f"File not found: {input_file_path}")
